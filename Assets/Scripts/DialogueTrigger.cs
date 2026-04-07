8950ed77a29abb495605c2f230f170e97c2df91b
Beantea Developer 1 <beanteastudios@gmail.com>
2026-04-07 19:30:45 +0100

    "Interaction prompt working - appears in range, hides during dialogue, reappears after"

--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -10,19 +10,23 @@ public class DialogueTrigger : MonoBehaviour
     public Transform player;
     public DialogueRunner dialogueRunner;
     public TMP_Text speakerNameText;
+    public GameObject interactionPrompt;
 
     private bool hasTriggered = false;
-
-    void Start()
-    {
-        if (dialogueRunner != null)
-            dialogueRunner.onDialogueComplete.AddListener(OnDialogueEnded);
-    }
 
     void OnEnable()
     {
         if (speakerNameText != null)
             speakerNameText.color = new Color(1f, 0.78f, 0.39f, 1f);
+
+        if (dialogueRunner != null)
+            dialogueRunner.onDialogueComplete.AddListener(OnDialogueEnded);
+    }
+
+    void OnDisable()
+    {
+        if (dialogueRunner != null)
+            dialogueRunner.onDialogueComplete.RemoveListener(OnDialogueEnded);
     }
 
     void Update()
@@ -34,11 +38,16 @@ public class DialogueTrigger : MonoBehaviour
             player.position
         );
 
+        if (interactionPrompt != null)
+            interactionPrompt.SetActive(distance < interactRange);
+
         if (distance < interactRange)
         {
             if (Keyboard.current.eKey.wasPressedThisFrame)
             {
                 hasTriggered = true;
+                if (interactionPrompt != null)
+                    interactionPrompt.SetActive(false);
                 dialogueRunner.StartDialogue(nodeName);
             }
         }
@@ -48,12 +57,6 @@ public class DialogueTrigger : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, interactRange);
-    }
-
-    void OnDisable()
-    {
-        if (dialogueRunner != null)
-            dialogueRunner.onDialogueComplete.RemoveListener(OnDialogueEnded);
     }
 
     void OnDialogueEnded()
